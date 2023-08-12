89561232458ce8693d53226a4c724181f8d53c3c
Gregory Melsby <greg27426@gmail.com>
2023-08-11 20:18:26 -0400

    basic fetching works

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const lightAddress = import.meta.env.VITE_LIGHT_ADDRESS;
+
+export default function App() {
+  const [lightState, setLightState] = useState("Loading...")
+
+  // fetch state upon page load
+  useEffect(() => {
+    fetch(lightAddress)
+      .then(async response => {
+        if (!response.ok) {
+          throw new Error('issue fetching state');
+        }
+        const responseBody = await response.json();
+        setLightState(responseBody.state)
+      })
+      .catch(error => {
+        console.error(error);
+        return;
+      });
+  }, [setLightState])
 
   return (
-    <>
+    <div >
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>{lightState}</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        <button>Off</button>
+        <button>On Air</button>
+        <button>On Camera</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
-
-export default App
