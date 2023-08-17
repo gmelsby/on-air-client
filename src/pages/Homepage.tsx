c5c9d539014367615d121074f2d3e4577a5d14be
Gregory Melsby <greg27426@gmail.com>
2023-08-17 11:33:18 -0400

    modified page layout to space buttons and status

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -32,34 +32,36 @@ export default function HomePage({lightState, updateState, isConnected}:
   return (
   <>
     <BackgroundColorChanger {...{lightState}} />
-    <div className="text-center p-3 my-3">
-      <h1 className="text-3xl text-white font-bold text-stroke">{lightState === undefined || ! isConnected ?
-        'Attempting to connect...' : 
-        displayMap.get(lightState)}
-      </h1>
+    <div className="flex flex-col justify-around align-middle">
+      <div className="text-center pt-36">
+        <h1 className="text-6xl text-white font-bold text-stroke">{lightState === undefined || ! isConnected ?
+          'Attempting to connect...' : 
+          displayMap.get(lightState)}
+        </h1>
+      </div>
+      {isConnected && lightState !== 'offline' &&
+        <div className='flex flex-col md:flex-row justify-center'>
+          {lightState !== 'off' &&
+            <button className="btn bg-gray-600 m-2 text-stroke"
+              onClick={() => attemptUpdate(LightCategory.Off)}>
+              Off
+            </button>
+          }
+          {lightState !== 'on-air' &&
+            <button className="btn bg-red-600 m-2 text-stroke"
+              onClick={() => attemptUpdate(LightCategory.OnAir)}>
+              On Air
+            </button>
+          }
+          {lightState !== 'on-camera' &&
+            <button className="btn bg-blue-600 m-2 text-stroke"
+              onClick={() => attemptUpdate(LightCategory.OnCamera)}>
+              On Camera
+            </button>
+          }
+        </div>
+      }
     </div>
-    {isConnected && lightState !== 'offline' &&
-    <div className='flex flex-col md:flex-row justify-center'>
-      {lightState !== 'off' &&
-        <button className="btn bg-gray-600 m-2 text-stroke"
-          onClick={() => attemptUpdate(LightCategory.Off)}>
-          Off
-        </button>
-      }
-      {lightState !== 'on-air' &&
-        <button className="btn bg-red-600 m-2 text-stroke"
-          onClick={() => attemptUpdate(LightCategory.OnAir)}>
-          On Air
-        </button>
-      }
-      {lightState !== 'on-camera' &&
-        <button className="btn bg-blue-600 m-2 text-stroke"
-          onClick={() => attemptUpdate(LightCategory.OnCamera)}>
-          On Camera
-        </button>
-      }
-    </div>
-    }
   </>
   );
 }
