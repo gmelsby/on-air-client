9f77ffd602986325df467021bc25d9282fbf9862
Gregory Melsby <greg27426@gmail.com>
2023-08-17 11:56:51 -0400

    added inner shadow to buttons

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -33,8 +33,9 @@ export default function HomePage({lightState, updateState, isConnected}:
   <>
     <BackgroundColorChanger {...{lightState}} />
     <div className="flex flex-col justify-around align-middle">
-      <div className="text-center pt-36">
-        <h1 className="text-6xl text-white font-bold text-stroke">{lightState === undefined || ! isConnected ?
+      <div className={`text-center${lightState === 'offline' || !isConnected ? '' :' pt-36'}`}>
+        <h1 className="text-6xl text-white font-bold text-stroke">
+          {lightState === undefined || ! isConnected ?
           'Attempting to connect...' : 
           displayMap.get(lightState)}
         </h1>
@@ -42,19 +43,25 @@ export default function HomePage({lightState, updateState, isConnected}:
       {isConnected && lightState !== 'offline' &&
         <div className='flex flex-col md:flex-row justify-center'>
           {lightState !== 'off' &&
-            <button className="btn bg-gray-600 m-2 text-stroke"
+            <button className="btn bg-gray-600 m-2 text-stroke text-xl
+            shadow-inner shadow-gray-300
+            hover:bg-gray-800"
               onClick={() => attemptUpdate(LightCategory.Off)}>
               Off
             </button>
           }
           {lightState !== 'on-air' &&
-            <button className="btn bg-red-600 m-2 text-stroke"
+            <button className="btn bg-red-600 m-2 text-stroke text-xl
+            shadow-inner shadow-red-300
+            hover:bg-red-800"
               onClick={() => attemptUpdate(LightCategory.OnAir)}>
               On Air
             </button>
           }
           {lightState !== 'on-camera' &&
-            <button className="btn bg-blue-600 m-2 text-stroke"
+            <button className="btn bg-blue-600 m-2 text-stroke text-xl
+            shadow-inner shadow-blue-300
+            hover:bg-blue-800"
               onClick={() => attemptUpdate(LightCategory.OnCamera)}>
               On Camera
             </button>
