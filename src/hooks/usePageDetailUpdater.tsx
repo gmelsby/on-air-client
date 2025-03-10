d46da3a9ebae76f95d16cd80e794e4082564a657
Gregory Melsby <greg27426@gmail.com>
2025-03-10 19:01:21 -0400

    added lightbulb asset

--- src/hooks/usePageDetailUpdater.tsx
+++ src/hooks/usePageDetailUpdater.tsx
@@ -3,13 +3,10 @@ import { LightCategory } from "../LightCategory";
 
 // map from LightCategory to css background color class and favicon
 const detailsMap = new Map([
-  [LightCategory.Off, { backgroundColor: "bg-neutral-800", favicon: "⚪️" }],
-  [LightCategory.OnAir, { backgroundColor: "bg-red-950", favicon: "🔴" }],
-  [LightCategory.OnCamera, { backgroundColor: "bg-blue-950", favicon: "🔵" }],
-  [
-    LightCategory.Offline,
-    { backgroundColor: "black-background", favicon: "⚫️" },
-  ],
+  [LightCategory.Off, { favicon: "⚪️" }],
+  [LightCategory.OnAir, { favicon: "🔴" }],
+  [LightCategory.OnCamera, { favicon: "🔵" }],
+  [LightCategory.Offline, { favicon: "⚫️" }],
 ]);
 
 export default function PageDetailUpdater(
@@ -19,8 +16,6 @@ export default function PageDetailUpdater(
   useEffect(() => {
     const updateDetails = detailsMap.get(lightState as LightCategory);
     if (updateDetails !== undefined) {
-      // update background
-      document.body.className = updateDetails["backgroundColor"];
       // update favicon
       const favSVG = `data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.99em%22 font-size=%2280%22>${updateDetails["favicon"]}</text></svg>`;
       const favElement = document.getElementById("favicon");
